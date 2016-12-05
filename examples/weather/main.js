df74c009c4fbf1b72b583c191cc16c93f4b75373
farhan687 <farhan687@gmail.com>
2016-12-05 16:50:20 +0530

    Use heatmap in weather example

--- examples/weather/main.js
+++ examples/weather/main.js
@@ -9,6 +9,59 @@ import {ReactiveMap,
 class Main extends Component {
 	constructor(props) {
 		super(props);
+		this.markerOnIndex = this.markerOnIndex.bind(this);
+	}
+	getGradient(temp) {
+		if(temp <= 5) {
+			return 'rgba(0, 0, 255, 1)';
+		}
+		else if(temp > 5 && temp <= 10) {
+			return 'rgba(255,255,0, 1)';
+		}
+		else if(temp > 10 && temp <= 15) {
+			return 'rgba(128,0,128, 1)';
+		}
+		else if(temp > 15 && temp <= 20) {
+			return 'rgba(255,0,255, 1)';
+		}
+		else if(temp > 20) {
+			return 'rgba(255,0,0, 1)';
+		}
+	}
+	markerOnIndex(res) {
+		if (res.allMarkers && res.allMarkers.hits && res.allMarkers.hits.hits) {
+			let result = res.allMarkers.hits.hits.map((markerData, index) => {
+				let location = markerData._source[this.props.mapping.location];
+				return new google.maps.LatLng(location.lat, location.lon);
+			});
+			let gredient = res.allMarkers.hits.hits.map((markerData, index) => {
+				let source = markerData._source;
+				return this.getGradient(source.main.temp);
+			});
+			console.log(gredient);
+			let heatmap = new google.maps.visualization.HeatmapLayer({
+				data: result,
+				map: res.mapRef.props.map,
+				radius: 50
+			});
+			var gradient = [
+				'rgba(0, 255, 255, 0)',
+				'rgba(0, 255, 255, 1)',
+				'rgba(0, 191, 255, 1)',
+				'rgba(0, 127, 255, 1)',
+				'rgba(0, 63, 255, 1)',
+				'rgba(0, 0, 255, 1)',
+				'rgba(0, 0, 223, 1)',
+				'rgba(0, 0, 191, 1)',
+				'rgba(0, 0, 159, 1)',
+				'rgba(0, 0, 127, 1)',
+				'rgba(63, 0, 91, 1)',
+				'rgba(127, 0, 63, 1)',
+				'rgba(191, 0, 31, 1)',
+				'rgba(255, 0, 0, 1)'
+			];
+			heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+		}
 	}
 	render() {
 		return (
@@ -17,18 +70,20 @@ class Main extends Component {
 				<div className="col s12 h-100">
 					<AppbaseMap
 						inputData={this.props.mapping.location}
-						defaultZoom={11}
+						defaultZoom={4}
 						defaultCenter={{ lat: 40.673940, lng: -101.314026 }}
 						historicalData={true}
 						markerCluster={false}
 						searchComponent="appbase"
 						searchField={this.props.mapping.venue}
 						mapStyle={this.props.mapStyle}
+						markerOnIndex={this.markerOnIndex}
 						autoCenter={false}
 						size={100}
 						searchAsMoveComponent={true}
 						searchAsMoveDefault={true}
 						MapStylesComponent={true}
+						allowMarkers={false}
 						title="Meetupblast"
 						/>
 				</div>
